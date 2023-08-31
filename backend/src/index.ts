e92fda0e7cd7c5738faecd99f4135cb28f43b9f3
Daniel Angeles <pfbarrett98@gmail.com>
2023-08-31 12:32:25 -0500

    (backend): Add typeDeft to apollo-server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,85 @@ app.get('/', (req, res) => {
   res.send('Buenas noches, Funciona!')
 })
 
+const typeDeft = `#graphql
+  type Info {
+    count: Int
+    pages: Int
+    next: Int
+    prev: Int
+  }
+
+  type Character {
+    id: Int
+    name: String
+    status: String
+    species: String
+    type: String
+    gender: String
+    origin: Origin
+    location: Location
+    image: String
+    episode: [Episode]!
+    created: String
+  }
+  
+  type Characters: {
+    info: Info
+    results: [Character]
+  }
+
+  type FiltersCharacter {
+    name: String
+    status: String
+    species: String
+    type: String
+    gender: String
+  }
+
+  type Location {
+    id: Int
+    name: String
+    type: String
+    dimension: String
+    residents: [Character]!
+    created: String
+  }
+
+  type Locations {
+    info: Info
+    results: [Location]
+  }
+
+  type FiltersLocation {
+    name: String
+    type: String
+    dimension: String
+  }
+
+  type Episode {
+    id: ID
+    name: String
+    air_date: String
+    episode: String
+    characters: [Character]!
+    created: String
+  }
+
+  type Episodes {
+    info: Info
+    results: [Episode]
+  }
+
+  type FiltersEpisode {
+    name: String
+    episode: String
+  }
+
+  type Query {
+    character(id: ID!): Character
+  }
+`
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
